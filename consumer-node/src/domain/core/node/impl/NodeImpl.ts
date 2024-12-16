a1171284905786516fc1b802e2312f016a688613
Alberto Paganelli <albi1600@gmail.com>
2024-12-16 14:22:07 +0100

    chore: add init transport method to consumer node impl

--- consumer-node/src/domain/core/node/impl/NodeImpl.ts
+++ consumer-node/src/domain/core/node/impl/NodeImpl.ts
@@ -5,11 +5,14 @@ import {ProviderEventsHub} from "../../../../infrastructure/events/impl/Provider
 import {TransportManager} from "../../../../infrastructure/transport/TransportManager.js";
 import {Socks5TransportManager} from "../../../../infrastructure/transport/socks5/Socks5TransportManager.js";
 import {Socks5Transport} from "../../../../infrastructure/transport/socks5/Socks5Transport.js";
-import {TaskEvent} from "../../../events/task/TaskEvent";
+import {TaskEvent} from "../../../events/task/TaskEvent.js";
+import {SocketTransportManager} from "../../../../infrastructure/transport/socket/SocketTransportManager.js";
+import {SocketTransport} from "../../../../infrastructure/transport/socket/SocketTransport.js";
 
 config({path: process.cwd() + '/../.env'})
 
 export class NodeImpl implements Node {
+    private readonly anonymousMode: boolean = process.env.ANONYMOUS_MODE === "true";
     private readonly bootstrapNodes: Map<string, number>;
     private readonly address: string;
     private readonly port: number;
@@ -21,13 +24,26 @@ export class NodeImpl implements Node {
         this.providerEventsHub = new ProviderEventsHub();
         this.address = address;
         this.port = port
-        this.transportManager = new Socks5TransportManager(
-            new Socks5Transport({
-                    addressMap: this.bootstrapNodes,
-                },
-                this.providerEventsHub.routeEvent.bind(this.providerEventsHub))
-        );
+        this.transportManager = this.initTransport()
         this.providerEventsHub.useTransport(this.transportManager)
+    }
+
+    private initTransport(): TransportManager {
+        if(this.anonymousMode) {
+            return new Socks5TransportManager(
+                new Socks5Transport({
+                        addressMap: this.bootstrapNodes,
+                    },
+                    this.providerEventsHub.routeEvent.bind(this.providerEventsHub))
+            );
+        } else {
+            return new SocketTransportManager(
+                new SocketTransport({
+                        addressMap: this.bootstrapNodes,
+                    },
+                    this.providerEventsHub.routeEvent.bind(this.providerEventsHub))
+            );
+        }
     }
 
     async submitTask(taskEvent: TaskEvent): Promise<void> {
