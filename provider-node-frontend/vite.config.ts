ac92f238bc3ca96c51b41675ce09a43fed7bf8fc
Alberto Paganelli <albi1600@gmail.com>
2025-01-17 15:40:44 +0100

    wip: change env variable detection for vite in provider frontend

--- provider-node-frontend/vite.config.ts
+++ provider-node-frontend/vite.config.ts
@@ -1,35 +1,42 @@
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import vue from '@vitejs/plugin-vue';
+import { defineConfig, loadEnv } from 'vite';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [
-    // the resolving is in tsconfig.shared.json using this plugin
-    tsconfigPaths(),
-    nodePolyfills({
-      exclude: [
-        'domain' // Excludes the polyfill for `http` and `node:http`.
-      ]
-    }),
-
-    vue({
-      template: { transformAssetUrls }
-    }),
-    // @quasar/plugin-vite options list:
-    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
-    quasar({
-      sassVariables: 'src/assets/quasar-variables.sass'
-    })
-  ],
-  server: {
-    port: 8080
-  },
-  preview: {
-    port: 8080
-  },
-  envDir: '../'
-})
+export default defineConfig(({ mode }) => {
+  // Load environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd());
+  return {
+    base: './',
+    plugins: [
+      // Resolving paths as defined in tsconfig.shared.json
+      tsconfigPaths(),
+      nodePolyfills({
+        exclude: [
+          'domain' // Excludes the polyfill for `http` and `node:http`.
+        ]
+      }),
+      vue({
+        template: { transformAssetUrls }
+      }),
+      quasar({
+        sassVariables: 'src/assets/quasar-variables.sass'
+      })
+    ],
+    server: {
+      port: 8080
+    },
+    preview: {
+      port: 8080
+    },
+    envDir: '../',
+    define: {
+      VITE_PROVIDER_NODE_HOST: JSON.stringify(env.VITE_PROVIDER_NODE_HOST || 'localhost'),
+      VITE_PROVIDER_NODE_PORT: JSON.stringify(env.VITE_PROVIDER_NODE_PORT || ''),
+      VITE_DEV_API_KEY: JSON.stringify(env.VITE_DEV_API_KEY || ''),
+      VITE_ANONYMOUS_MODE: JSON.stringify(env.VITE_ANONYMOUS_MODE || '')
+    }
+  };
+});
